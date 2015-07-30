d8b71f8ba109c0ad8dc948cdfc2e388b26ba408f
Hasan Adil <@dispatch_hasan>
2015-07-30 07:12:05 -0400

    Initial Commit

---
+++ HACollections/HACollections.h
@@ -0,0 +1,13 @@
+//
+//  HACollections.h
+//  HACollections
+//
+//  Created by Hasan Adil on 7/30/15.
+//  Copyright (c) 2015 Hasan Adil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HACollections : NSObject
+
+@end
---
+++ HACollections/HACollections.m
@@ -0,0 +1,13 @@
+//
+//  HACollections.m
+//  HACollections
+//
+//  Created by Hasan Adil on 7/30/15.
+//  Copyright (c) 2015 Hasan Adil. All rights reserved.
+//
+
+#import "HACollections.h"
+
+@implementation HACollections
+
+@end
